21c00c588a2b4b6e6d881c28109fc5cafd719772
p2424431 <gyxk01.24@ichat.sp.edu.sg>
2025-03-15 22:30:34 +0800

    Creating a React front-end

---
+++ .github/front-end/src/pages/ArticlesListPage.jsx
@@ -0,0 +1,11 @@
+import ArticleList from "./ArticleList";
+import articles from "../article-content";
+
+export default function ArticlesListPage() {
+  return (
+  <>
+  <h1>Articles</h1>
+  <ArticleList articles = {articles} />
+  </>
+  );
+}
