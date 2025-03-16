045862cc07f7b7378541e1406870c9aa0ada39a8
p2424431 <gyxk01.24@ichat.sp.edu.sg>
2025-03-16 15:55:10 +0800

    Creating a Node.js Back-End

---
+++ .github/back-end/src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const articleInfo = [
+  { name: "learn-node", upvotes: 0, comments: [] },
+  { name: "learn-react", upvotes: 0, comments: [] },
+  { name: "mongodb", upvotes: 0, comments: [] },
+];
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const article = articleInfo.find((a) => a.name === req.params.name);
+  article.upvotes += 1;
+
+  res.json(article);
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articleInfo.find((a) => a.name === name);
+
+  article.comments.push({
+    postedBy,
+    text,
+  });
+
+  res.json(article);
+});
+
+app.listen(8000, function () {
+  console.log("Server is listening on port 8000");
+});
